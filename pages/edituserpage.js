1c4b88ec26b3b8f0160ffefed47f7661d5094285
stephanyanndelapena <sasdelapena.student@ua.edu.ph>
2025-10-15 11:13:38 +0800

    Edit User Page

--- pages/edituserpage.js
+++ pages/edituserpage.js
@@ -1,10 +1,80 @@
-import {View, Text} from "react-native";
+import {View, Text, Button, TextInput} from "react-native";
+import {useState} from 'react';
+import axios from 'axios';
 import styles from '../styles';
 
-export default function EditUserPage() {
+export default function EditUserPage({route, navigation}) {
+    const {user} = route.params;
+
+    const [firstname, setFirstName] = useState(user.first_name);
+    const [lastname, setLastName] = useState(user.last_name);
+    const [user_email, setUserEmail] = useState(user.email);
+    const [user_gender, setUserGender] = useState(user.gender);
+    const [user_password, setUserPassword] = useState(user.password);
+
+    const handleUpdate = () => {
+        if (!firstname || !lastname || !user_email || !user_gender || !user_password){
+            window.alert("Error", "Please fill up all required fields");
+            return;
+        }
+
+        axios
+            .put(`http://127.0.0.1:8000/registration/api/users/${user.id}/`, {
+                first_name: firstname,
+                last_name: lastname,
+                email: user_email,
+                gender: user_gender,
+                password: user_password,
+            })
+            .then(() => {
+                window.alert("Success", "User updated successfully!");
+                navigation.goBack();
+            })
+            .catch((error) => {
+                console.error(error);
+                window.alert("Error", "Failed to update");
+            });
+
+    };
     return (
         <View style={styles.container}>
             <Text style={styles.homepageText}>Edit User</Text>
+
+            <TextInput
+            value={firstname}
+            onChangeText={setFirstName}
+            style={styles.input}
+            placeholder="First Name"
+            />
+            <TextInput
+            value={lastname}
+            onChangeText={setLastName}
+            style={styles.input}
+            placeholder="Last Name"
+            />
+            <TextInput
+            value={user_email}
+            onChangeText={setUserEmail}
+            style={styles.input}
+            placeholder="Email"
+            keyboardType="email-address"
+            />
+            <TextInput
+            value={user_gender}
+            onChangeText={setUserGender}
+            style={styles.input}
+            placeholder="Gender"
+            />
+            <TextInput
+            value={user_password}
+            onChangeText={setUserPassword}
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry={true}
+            />
+            <View style={styles.buttonWrapper}>
+                <Button title="Update User" color="#c954c3ff" onPress={handleUpdate} />
+            </View>
         </View>
     );
 }
--- pages/userlistpage.js
+++ pages/userlistpage.js
@@ -22,28 +22,28 @@ export default function UserListPage({navigation}) {
     }
 
     const handleDelete = (id) => {
-        // Alert.alert(
-        //     "Confirm Delete",
-        //     "Are you sure you want to delete this user?",
-        //     (
-        //         { text: "Cancel", style: "cancel" },
-        //         { 
-        //             text: "Delete",
-        //             style: "destructive",
-        //             onPress: () => {
-        //                 axios.delete(`http://127.0.0.1:8000/registration/api/users/${id}/`)
-        //                 .then(() => {
-        //                     Alert.alert("Success", "User deleted successfully");
-        //                 })
-        //                 .catch((err) => {
-        //                     console.error(err);
-        //                     Alert.alert("Error", "Failed to delete user");
-        //                 });
+        window.alert(
+            "Confirm Delete",
+            "Are you sure you want to delete this user?",
+            (
+                { text: "Cancel", style: "cancel" },
+                { 
+                    text: "Delete",
+                    style: "destructive",
+                    onPress: () => {
+                        axios.delete(`http://127.0.0.1:8000/registration/api/users/${id}/`)
+                        .then(() => {
+                            window.alert("Success", "User deleted successfully");
+                        })
+                        .catch((err) => {
+                            console.error(err);
+                            window.alert("Error", "Failed to delete user");
+                        });
 
-        //             },
-        //         }
-        //     )
-        // );
+                    },
+                }
+            )
+        );
         
         axios.delete(`http://127.0.0.1:8000/registration/api/users/${id}/`)
     };
